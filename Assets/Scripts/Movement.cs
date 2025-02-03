1f836ed6ca4d4500731f814b6dfa83f81485de78
VICTORIA MARABOTO <VMARABOTO84246@uat.edu>
2025-02-02 22:41:46 -0700

    Coded and tested the sprite movement and exit feature

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -1,9 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
+    // Speed of Movement
+    public float speed = 5f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +17,52 @@ public class Movement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        // Calls movement function
+        MovePlanet();
+
+        // Exits game when esc is pressed
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Quit();
+        }
+    }
+
+    // Movement function
+    void MovePlanet()
+    {
+        // X and Y variables
+        float moveX = 0f;
+        float moveY = 0f;
+
+        // Key detection
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            moveY = 1f;
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            moveY = -1f;
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            moveX = -1f;
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            moveX = 1f;
+        }
+
+        // Movement application
+        Vector2 movePosition = new Vector2(moveX, moveY).normalized;
+        transform.position += (Vector3)movePosition * speed * Time.deltaTime;
+    }
+
+    void Quit()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+    Application.Quit();
+#endif
     }
 }
